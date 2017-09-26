5a310379ae344bf7ecb72ddb03ef35984445405b
AleksGram <https://github.com/AleksGram/builder.git>
2017-09-26 23:44:48 +0300

    added tabs

--- chapter_05/01_expandable_menus/kinomonstr/script.js
+++ chapter_05/01_expandable_menus/kinomonstr/script.js
@@ -36,11 +36,23 @@ $(this)
 }
  $(this)
       .find('.acord_content:first')
-     // .stop(true, true)
+      .stop(true, true)
       .slideToggle();
 
 });
 
+$('#info p:not(:first)').hide();
+
+$('#info-nav li').click(function(event){
+event.preventDefault();
+$('#info p').hide();
+$('#info-nav .current').removeClass("current");
+$(this).addClass('current');
+
+var clicked = $(this).find('a:first').attr('href');
+$('#info '+ clicked).fadeIn('fast');
+
+}).eq(0).addClass('current');
 
 
 
