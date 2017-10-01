e5c0bb1b2c9219bcba285df404388fdbed13defe
AleksGram <https://github.com/AleksGram/builder.git>
2017-10-01 20:50:53 +0300

    added cart + overlay

--- chapter_05/01_expandable_menus/kinomonstr/script.js
+++ chapter_05/01_expandable_menus/kinomonstr/script.js
@@ -65,13 +65,36 @@ spinner: 'Loading.....',
 cache: true
 });
 
-$('#info').tabs().tabs('rotate', 3500);
+/*$('#info').tabs().tabs('rotate', 3500);*/
+
+/*---------overlay------------------*/
+$('<div></div>')
+        .attr('id', 'overlay')
+        .css('opacity', 0.65)
+        .hover(function(){
+          $(this).addClass('active');
+        }, function() {
+          $(this).removeClass('active');
+          setTimeout(function(){
+            $('#overlay:not(.active)').slideUp(function(){
+              $('a.cart-hover').removeClass('cart-hover');
+            });
+          }, 800);
+        }).appendTo('body');
 
 
+
+
+$('.cart a').mouseover(function(){
+  $(this).addClass('cart-hover');
+  $('#overlay:not(:animated)')
+  .addClass('active')
+  .html('<h1>You have 5 items in your cart.</h1><a href="#">View Cart</a>&nbsp;<a href="#">Checkout</a>')
+  .slideDown();
+});
 
 
 });
-
 
 
 
