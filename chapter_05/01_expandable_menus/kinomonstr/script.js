f4d31aa1bf400fabc8712908ed20e984fb37b499
AleksGram <https://github.com/AleksGram/builder.git>
2017-10-03 23:41:12 +0300

    added tooltip

--- chapter_05/01_expandable_menus/kinomonstr/script.js
+++ chapter_05/01_expandable_menus/kinomonstr/script.js
@@ -93,7 +93,29 @@ $('.cart a').mouseover(function(){
   .slideDown();
 });
 
+$('.location').hover(function(event){
+    // Hover over code
+    var titleText = $(this).attr('title');
+    $(this)
+      .data('tipText', titleText)
+      .removeAttr('title');
 
+    $('<p class="tooltip"></p>')
+      .text(titleText)
+      .appendTo('body')
+      .css('top', (event.pageY - 10) + 'px')
+      .css('left', (event.pageX + 20) + 'px')
+      .fadeIn('slow');
+  }, function() {
+    // Hover out code
+    $(this).attr('title', $(this).data('tipText'));
+    $('.tooltip').remove();
+  }).mousemove(function(event){
+    // Mouse move code
+    $('.tooltip')
+      .css('top', (event.pageY - 10) + 'px')
+      .css('left', (event.pageX + 20) + 'px');
+  });
 });
 
 
